707d206f476da486a24cf65b4833b0ebdcc1e9aa
Cian Butler <ciandevelops@gmail.com>
2022-06-20 13:44:30 -0400

    navbar done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,13 +3,20 @@ import Link from "next/link";
 import React, { Fragment, useState } from "react";
 import Logo from "../public/assets/logo.png";
 import { Menu, Transition } from "@headlessui/react";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsPerson, BsSearch, BsThreeDotsVertical } from "react-icons/bs";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0E0E10] mb-[2px] z-10">
       {/* LEFTSIDE */}
@@ -98,8 +105,63 @@ const Navbar = () => {
           </Menu>
         </div>
       </div>
+
       {/* MIDDLE */}
+      <div className="hidden md:flex grow-[2] items-center justify-center">
+        <div className="bg-gray-500 text-white flex justify-between items-center max-w-[400px] w-full m-auto p-2 rounded-2xl">
+          <div className="">
+            <input
+              type="text"
+              className="bg-transparent border-none text-white focus:outline-none"
+              placeholder="Search"
+            />
+          </div>
+          <div>
+            <BsSearch />
+          </div>
+        </div>
+      </div>
+
       {/* RIGHTSIDE */}
+      <div className="hidden md:flex grow items-center justify-end">
+        <div className="flex items-center">
+          <Link href="/">
+            <button className="px-4 py-[6px] rounded-lg font-bold bg-[#9147FF] mr-2">
+              Account
+            </button>
+          </Link>
+          <BsPerson size={30} />
+        </div>
+      </div>
+
+      {/* Hamburger Menu */}
+      <div onClick={handleNav} className="block md:hidden z-10 cursor-pointer">
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      </div>
+
+      {/* MOBILE MENU */}
+      <div
+        className={
+          nav
+            ? "md:hidden fixed top-0 left-0 w-full h-screen bg-[#0E0E10] flex justify-center items-center ease-in duration-300"
+            : "md:hidden fixed top-[-100%] left-0 w-full h-screen bg-[#0E0E10] flex justify-center items-center ease-in duration-300"
+        }
+      >
+        <ul className="text-center">
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Live Channels</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Top Categories</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Account</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
