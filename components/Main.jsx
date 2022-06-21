08523533f4f3f9262aae64df2ea42385006e9d9c
Cian Butler <ciandevelops@gmail.com>
2022-06-20 20:26:11 -0400

    main layout done

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LiveChannels from "./LiveChannels";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="absolute left-[65px] xl:left-[240px]">
+      <LiveChannels />
+    </div>
+  );
 };
 
 export default Main;
