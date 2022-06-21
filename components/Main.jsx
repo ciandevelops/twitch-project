3615b32dbcdc33a687c452a931fe4bfa13512d34
Cian Butler <ciandevelops@gmail.com>
2022-06-20 21:06:51 -0400

    hero done

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Hero from "./Hero";
 import LiveChannels from "./LiveChannels";
 
 const Main = () => {
   return (
     <div className="absolute left-[65px] xl:left-[240px]">
+      <Hero />
       <LiveChannels />
     </div>
   );
